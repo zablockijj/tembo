368f0a7131b3a97d27fbac424d0042d7d1a4511f
Samir Mehta <sammehta88@gmail.com>
2016-07-11 17:09:23 -0400

    initialize app and create gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
+
+// below from recipe at https://github.com/gulpjs/gulp/blob/master/docs/recipes/mocha-test-runner-with-gulp.md
+gulp.task('test', function() {
+  gulp
+    .src(['test/*.js'], { read: false })
+    .pipe(mocha( { reporter: 'nyan' }))
+    .on('error', gutil.log);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./*.js'], ['test']);
+});
