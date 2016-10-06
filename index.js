afe2ee53fe5ace8444af28915ef5163f806896c3
Samir Mehta <sammehta88@gmail.com>
2016-10-06 06:51:30 -0400

    initial user auth  setup - not functional yet

--- index.js
+++ index.js
@@ -4,19 +4,42 @@
 var express = require('express');
 var wagner = require('wagner-core');
 var bodyParser = require('body-parser');
+var passport = require("passport");
+var session = require("express-session");
+var MongoStore = require('connect-mongo')(session);
 var smsRouter = require('./lib/routes/sms/routes.js');
+var webRouter = require('./lib/routes/web/routes.js');
 
 require('dotenv').load();
-require('./lib/models/models')(wagner);
+var models = require('./lib/models/models')(wagner);
+var connection = models.connection;
 
 var cronReminder = require('./lib/jobs/cron-reminder.js')(wagner);
+cronReminder.initialize();
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require('./lib/config/passport/local.js')(passport);
+
+app.use(session({
+  secret : 'chowCH0W',
+  store: new MongoStore({
+    mongooseConnection: connection
+  }),
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // routes for using app via text message
 app.use('/sms', smsRouter(wagner));
+app.use('/', webRouter(passport));
+
 
 // app.get('/', function(req, res) {
 //   wagner.invoke(function(Journal){
@@ -35,10 +58,10 @@ app.use('/sms', smsRouter(wagner));
 // });
 
 // app.get('/love', function(req, res) {
-//   twilioApp.sendSms('+12027668640', 'goodnight my love');
+//   twilioApp.sendSms('+', 'goodnight my love');
 //   res.send('goodnight my love');
 // });
-cronReminder.initialize();
+
 
 app.listen(3000);
 console.log('listening on port 3000');
